7401ff7c98ec002a5b5c33a54a8877a60a356d19
Robert Akers <akersrobb@outlook.com>
2015-10-23 18:47:22 -0500

    Updates

--- moocHelsinki/src/moocPartTwo.java
+++ moocHelsinki/src/moocPartTwo.java
@@ -1,10 +1,9 @@
+import java.util.Scanner;
 
 public class moocPartTwo {
 	public static void main(String[] args) {
 
+	
 
-	
-	
-	
 	}
 }
--- moocHelsinki/src/moocPartTwoExercises.java
+++ moocHelsinki/src/moocPartTwoExercises.java
@@ -1,10 +1,195 @@
+import java.util.Scanner;
 
 public class moocPartTwoExercises {
 	public static void main(String[] args) {
 
+		// Exercise 25
 
-	
-	
-	
+		/*
+		 * Scanner newScanner = new Scanner(System.in);
+		 * 
+		 * int sum = 0; int read;
+		 * 
+		 * System.out.println("Enter a number: "); sum =
+		 * Integer.parseInt(newScanner.nextLine());
+		 * 
+		 * System.out.println("Enter a number: "); sum = sum +
+		 * Integer.parseInt(newScanner.nextLine());
+		 * 
+		 * System.out.println("Enter a number: "); sum = sum +
+		 * Integer.parseInt(newScanner.nextLine());
+		 * 
+		 * System.out.println("Sum: " + sum);
+		 */
+
+		// Exercise 26
+		/*
+		 * Scanner newScanner = new Scanner(System.in); int sum = 0;
+		 * 
+		 * while(true){ int read = Integer.parseInt(newScanner.nextLine());
+		 * if(read == 0) { break; }else { sum = sum + read; System.out.println(
+		 * "The sum is: " + sum); } } System.out.println("The final sum is: " +
+		 * sum);
+		 */
+
+		// Exercise 27
+
+		int intttt = 1;
+		while (intttt <= 100) {
+			System.out.println(intttt);
+			intttt++;
+		}
+
+		// Exercise 28
+
+		int inttttt = 100;
+		while (inttttt >= 1) {
+			System.out.println(inttttt);
+			inttttt--;
+		}
+
+		// Exercise 29
+		int inttt = 2;
+		while (inttt <= 100) {
+			System.out.println(inttt);
+			inttt += 2;
+		}
+
+		// Exercise 30
+		/*
+		 * Scanner newScannerr = new Scanner(System.in); int intyint = 1;
+		 * System.out.println("Up to what number? "); int upTo =
+		 * Integer.parseInt(newScannerr.nextLine());
+		 * 
+		 * while (intyint <= upTo){ System.out.println(intyint); intyint++; }
+		 * 
+		 */
+
+		// Exercise 31
+
+		/*
+		 * Scanner newScanner = new Scanner(System.in); System.out.println(
+		 * "First: "); int intyint = Integer.parseInt(newScanner.nextLine());
+		 * 
+		 * System.out.println("Second: "); int intuint =
+		 * Integer.parseInt(newScanner.nextLine());
+		 * 
+		 * while(intyint <= intuint){ System.out.println(intyint); intyint++; }
+		 */
+
+		// Exercise 32
+
+		/*
+		 * Scanner newScanner = new Scanner(System.in);
+		 * 
+		 * System.out.println("Enter a number: "); int takeInt =
+		 * Integer.parseInt(newScanner.nextLine()); int i = 1; int getSum = 0;
+		 * 
+		 * while( i <= takeInt){ getSum = getSum + i; i++;
+		 * 
+		 * 
+		 * } System.out.println(getSum);
+		 */
+
+		// Exercise 33
+
+		/*
+		 * Scanner newScanner = new Scanner(System.in);
+		 * 
+		 * 
+		 * int firstVar, secondVar; int sumOf = 0;
+		 * 
+		 * 
+		 * System.out.println("Enter a number: "); firstVar =
+		 * Integer.parseInt(newScanner.nextLine());
+		 * 
+		 * System.out.println("Enter another number: "); secondVar =
+		 * Integer.parseInt(newScanner.nextLine());
+		 * 
+		 * 
+		 * while(firstVar <= secondVar) { sumOf = sumOf + firstVar; firstVar++;
+		 * }
+		 * 
+		 * 
+		 * System.out.println("The sum is: " + sumOf);
+		 */
+
+		// Exercise 34
+
+		/*
+		Scanner newScanner = new Scanner(System.in);
+
+		System.out.println("Type a number: ");
+		int takeInt = Integer.parseInt(newScanner.nextLine());
+
+		int i = 1;
+		int sumInt = 1
+				;
+		while (i <= takeInt) {
+			i++;
+			
+			sumInt = sumInt * i;
+			
+
+			if (i == takeInt) {
+				System.out.println(sumInt);
+				break;
+			}
+
+		}*/
+
+		
+		//Exercise 35
+		
+		
+		
+		
+		
+		Scanner newScanner = new Scanner(System.in);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 }
